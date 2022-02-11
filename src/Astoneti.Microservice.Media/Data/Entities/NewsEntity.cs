f53b0ab61e3ca2f7274e4ad767de84f924d1ae75
GODELTECH\a.pashkun <a.pashkun@godeltech.com>
2022-02-11 18:03:02 +0300

    Added  Migration

--- src/Astoneti.Microservice.Media/Data/Entities/NewsEntity.cs
+++ src/Astoneti.Microservice.Media/Data/Entities/NewsEntity.cs
@@ -4,6 +4,8 @@ namespace Astoneti.Microservice.Media.Data.Entities
 {
     public class NewsEntity
     {
+        public int Id { get; set; }
+
         public string Title { get; set; }
 
         public string Body { get; set; }
