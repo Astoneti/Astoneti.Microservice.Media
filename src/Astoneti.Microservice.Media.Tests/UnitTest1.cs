579ad005920f7e2fe1786632c9db3fdedbdfefa9
GODELTECH\a.pashkun <a.pashkun@godeltech.com>
2022-02-11 14:51:32 +0300

    Added solution stucture

---
+++ src/Astoneti.Microservice.Media.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Astoneti.Microservice.Media.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
