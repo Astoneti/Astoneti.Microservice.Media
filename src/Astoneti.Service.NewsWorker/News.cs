ef1a4c3c6087ae693526dec2c565409b19a7a32a
GODELTECH\a.pashkun <a.pashkun@godeltech.com>
2022-02-22 12:34:22 +0300

    Added GetDataFromAPI for geting list of  News fom API via console

---
+++ src/Astoneti.Service.NewsWorker/News.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Astoneti.Service.NewsWorker
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+    }
+}
--- src/Astoneti.Service.NewsWorker/Program.cs
+++ src/Astoneti.Service.NewsWorker/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace Astoneti.Service.NewsWorker
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new();
+
+        private static async Task<IList<News>> GetDataFromAPI()
         {
-            Console.WriteLine("Hello World!");
+            return await client.GetFromJsonAsync<IList<News>>("https://localhost:5001/news");
+        }
+
+        private static async Task<IList<News>> PostDataFromConsoleToAPI()
+        {
+            return await client.PostAsJsonAsync<News>("https://localhost:5001/news");
+        }
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("API import starting...");
+
+                var news = await GetDataFromAPI();
+
+                foreach (var n in news)
+                    Console.WriteLine(
+                       $"News List : { n.Id }, " +
+                       $"{ n.Title }, " +
+                       $"{ n.Body }, " +
+                       $"{ n.PublicationDate }"
+                 );
+
+                Console.WriteLine(
+                    "Total News is : " 
+                    + news.Count
+                 );
+
+                Console.WriteLine(
+                    "API import ended successfully"
+                 );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(
+                    "Error : " 
+                    + ex.Message
+                 );
+            }
         }
     }
 }
